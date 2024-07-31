7110ed00b9c3cab68d23e52b623b6b9a3efdc3ad
YoungYoon <duddbs1994@naver.com>
2024-07-31 12:04:17 +0900

    충돌 테스트

--- src/main/java/TikkeulMountainApp/Main.java
+++ src/main/java/TikkeulMountainApp/Main.java
@@ -3,6 +3,6 @@ package TikkeulMountainApp;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!!");
     }
 }
