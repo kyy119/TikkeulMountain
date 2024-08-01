01ff6288b30c81d2d4c50659e0f4f5bfb864a634
suhueon <degree.h0528@gmail.com>
2024-08-01 09:13:49 +0900

    PartyService 생성 및 코드 만듬

--- src/main/java/TikkeulMountainApp/party/Party.java
+++ src/main/java/TikkeulMountainApp/party/Party.java
@@ -13,4 +13,15 @@ public class Party {
     private String party_account_created_at;
     private String category;
 
+    public Party(String party_name, int daily_pay, String party_account,
+        String party_account_password,
+        int party_account_balance, String party_account_created_at, String category) {
+        this.party_name = party_name;
+        this.daily_pay = daily_pay;
+        this.party_account = party_account;
+        this.party_account_password = party_account_password;
+        this.party_account_balance = party_account_balance;
+        this.party_account_created_at = party_account_created_at;
+        this.category = category;
+    }
 }
