d2243aac61ac218e8d2ec8eff1d6eadd359d3dd3
suhueon <degree.h0528@gmail.com>
2024-08-01 18:34:12 +0900

    Party 필드 대쉬 지움 수정

--- src/main/java/TikkeulMountainApp/party/PartyService.java
+++ src/main/java/TikkeulMountainApp/party/PartyService.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class PartyService {
 
@@ -29,21 +30,21 @@ public class PartyService {
                 "목돈","1");
             //PreparedStatement 얻기 및 값 지정
             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, party.getParty_name());
-            pstmt.setInt(2, party.getDaily_pay());
-            pstmt.setString(3, party.getParty_account());
-            pstmt.setString(4, party.getParty_account_password());
-            pstmt.setInt(5, party.getParty_account_balance()); //10만
-            pstmt.setString(6, party.getParty_account_created_at());
+            pstmt.setString(1, party.getPartyName());
+            pstmt.setInt(2, party.getDailyPay());
+            pstmt.setString(3, party.getPartyAccount());
+            pstmt.setString(4, party.getPartyAccountPassword());
+            pstmt.setInt(5, party.getPartyAccountBalance()); //10만
+            pstmt.setString(6, party.getPartyAccountCreatedAt());
             pstmt.setString(7, party.getCategory());
-            pstmt.setString(8, party.getParty_active());
+            pstmt.setString(8, party.getPartyActive());
 
-            System.out.println(party.getParty_name());
-            party.getDaily_pay();
-            party.getParty_account();
-            party.getParty_account_password();
-            party.getParty_account_balance();
-            party.getParty_account_created_at();
+            System.out.println(party.getPartyName());
+            party.getDailyPay();
+            party.getPartyAccount();
+            party.getPartyAccountPassword();
+            party.getPartyAccountBalance();
+            party.getPartyAccountCreatedAt();
             party.getCategory();
 
             //SQL 문 실행
@@ -144,6 +145,121 @@ public class PartyService {
         return 0;
 
     }
+    public static ArrayList<Party> showPartyList(String userId) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        String sql = "SELECT party_id, party_name, party_active " + "FROM PARTY " +
+            "WHERE party_id IN " +
+            "(SELECT party_id FROM MEMBERSHIP WHERE user_id IN " +
+            "(SELECT user_id FROM `USER` WHERE user_id = ?))";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, userId);
+        ResultSet rs = pstmt.executeQuery();
+        ArrayList<Party> arr = new ArrayList<>();
+        while (rs.next()) {
+            Party party = new Party();
+            party.setPartyId(rs.getInt("party_id"));
+            party.setPartyName(rs.getString("party_name"));
+            String active = rs.getString("party_active");
+            if (active.equals("1")) {
+                party.setPartyActive(active);
+                arr.add(party);
+            } else {
+                continue;
+            }
+        }
+        return arr;
+    }
+    public static void searchParty(String userId, String str) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        ArrayList<Party> arr = showPartyList(userId);
+        ArrayList<String> arr2 = new ArrayList<>();
+        for (Party party : arr) {
+            if (party.getPartyName().contains(str)) {
+                arr2.add(party.getPartyName());
+            }
+        }
+        if (arr2.size() == 0) {
+            System.out.println("검색 내용이 없습니다.");
+        } else {
+            for (String str1 : arr2) {
+                System.out.println(str1);
+            }
+        }
+    }
+    public static void showPartyDetail(int partyId) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        String sql = "SELECT a.user_id, "
+            + "b.party_name, b.category, b.party_account, b.party_account_created_at "
+            + "from MEMBERSHIP as a"
+            + " INNER JOIN PARTY as b ON a.party_id = b.party_id"
+            + " WHERE b.party_id = ?;";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, partyId);
+        ResultSet rs = pstmt.executeQuery();
+        ArrayList<Party> arrayList = new ArrayList<>();
+        while (rs.next()) {
+            String userId = rs.getString("user_id");
+            String partyName = rs.getString("party_name");
+            String category = rs.getString("category");
+            String partyAccount = rs.getString("party_account");
+            String partyAccountCreatedAt = rs.getString("party_account_created_at");
+            Party party = new Party(userId, partyName, category, partyAccount,
+                partyAccountCreatedAt);
+            arrayList.add(party);
+        }
+        for (Party party : arrayList) {
+            System.out.println(party.getUserId());
+        }
+        System.out.println(arrayList.get(0).getPartyName());
+        System.out.println(arrayList.get(0).getCategory());
+        System.out.println(arrayList.get(0).getPartyAccount());
+        System.out.println(arrayList.get(0).getPartyAccountCreatedAt());
+    }
+    public static void deleteParty(String account) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        if (checkZero(account)) {
+            System.out.println("잔액이 있습니다. 삭제 불가");
+            return;
+        }
+        String sql = "DELETE FROM PARTY WHERE party_account = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, account);
+        pstmt.executeUpdate();
+        System.out.println("삭제 완료");
+    }
+    public static boolean checkZero(String account) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        //select party_account_balance from party where party_name = '김나나';
+        String sql = "SELECT party_account_balance FROM party WHERE party_account = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, account);
+        int price = 0;
+        ResultSet rs = pstmt.executeQuery();
+        if (rs.next()) {
+            price = rs.getInt("party_account_balance");
+        }
+        if (price == 0) {
+            return false;
+        }
+        return true;
+    }
+    public static void showParty(int id) throws SQLException {
+        Connection conn = MySqlConnect.MySqlConnect();
+        String sql = "SELECT party_name, party_account, party_account_balance FROM PARTY WHERE party_id = ?";
 
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+        ArrayList<String> arr = new ArrayList<>();
+        while (rs.next()) {
+            arr.add(rs.getString("party_name"));
+            arr.add(rs.getString("party_account"));
+            arr.add(Integer.toString(rs.getInt("party_account_balance")));
+        }
+
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(1));
+        System.out.println(arr.get(2));
+    }
 }
 
