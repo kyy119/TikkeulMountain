7a40b3eee44b0c8955202558d377467fcd6c4cf0
hoechanj <jhc400573@gmail.com>
2024-08-02 15:16:48 +0900

    user 조회, 로그인

--- src/main/java/TikkeulMountainApp/user/UserDao.java
+++ src/main/java/TikkeulMountainApp/user/UserDao.java
@@ -9,10 +9,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
-    // 회원가입 메서드
+    // Create 회원가입 메서드
     public static void registerUser(User user) {
         Connection conn = null;
-        String sql = "INSERT INTO USER (user_id, user_name, user_password, user_phone, user_account, user_account_balance, user_active) VALUES (?, ?, ?, ?, ?, ?,?)";
+        String sql = "INSERT INTO USER (user_id, user_name, user_password, user_phone, user_account, user_account_balance, user_active) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             conn = MySqlConnect.MySqlConnect();
             PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -34,26 +34,63 @@ public class UserDao {
         }
     }
 
-    public static void updateUserBalance(String userId, int amount) {
-        Connection conn = null; // Connection 객체 선언
-        String sql = "UPDATE USER SET user_account_balance = ? WHERE user_id = ?";
+    //로그인 메서드
+    public static boolean loginUser(String userid, String password) {
+        Connection conn = null;
+        String sql = "SELECT * FROM user WHERE user_id = ? AND user_password = ?";
+
         try {
             conn = MySqlConnect.MySqlConnect();
             PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.setInt(1, amount); // 잔액
-            pstmt.setString(2, userId); // 아이디
+            pstmt.setString(1, userid);
+            pstmt.setString(2, password);
 
-            int rowsUpdated = pstmt.executeUpdate();
-            if (rowsUpdated > 0) {
-                System.out.println("계좌 잔액이 성공적으로 수정되었습니다!");
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("로그인 성공!");
+                return true;
+            } else {
+                System.out.println("로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.");
+                return false;
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
 
     }
 
+    // Read 조회 메서드(사용자 정보 조회)
+    public static User getUser(String userId) {
+        Connection conn = null;
+        String sql = "SELECT * FROM USER WHERE user_id=?";
+        try {
+            conn = MySqlConnect.MySqlConnect();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, userId);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String userName = rs.getString("user_name");
+                String userPassword = rs.getString("user_password");
+                String userPhone = rs.getString("user_phone");
+                String userAccount = rs.getString("user_account");
+                int userAccountBalance = rs.getInt("user_account_balance");
+                String userActive = rs.getString("user_active");
+
+                User user = new User(userId, userName, userPassword, userPhone, userAccount, userAccountBalance, userActive);
+                System.out.println("사용자 정보 조회가 완료되었습니다!");
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 조회 메서드 (잔액 조회)
     public static int getUserBalance(String userId) {
         Connection conn = null;
         String sql = "SELECT user_account_balance FROM user WHERE user_id = ?";
@@ -77,15 +114,70 @@ public class UserDao {
         return 0;
     }
 
-    public static void updateUserActive(String userId, String useractive) {
+
+    //Update 수정 메서드 (잔액 수정)
+    public static void updateUserBalance(String userId, int amount) {
         Connection conn = null; // Connection 객체 선언
-        String sql = "UPDATE USER SET user_active = ? WHERE user_id = ?";
+        String sql = "UPDATE USER SET user_account_balance = ? WHERE user_id = ?";
         try {
             conn = MySqlConnect.MySqlConnect();
             PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.setString(1, useractive); // 활성화 0 or 1
-            pstmt.setString(2, userId); // 사용자 ID 설정
+            pstmt.setInt(1, amount); // 잔액
+            pstmt.setString(2, userId); // 아이디
+
+            int rowsUpdated = pstmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("계좌 잔액이 성공적으로 수정되었습니다!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //수정메서드 (비밀번호 수정)
+    public static void updateUserPassword(String userId, String newPassword) {
+        Connection conn = null;
+        String sql = "UPDATE USER SET user_password = ? WHERE user_id = ?";
+
+        try {
+            conn = MySqlConnect.MySqlConnect();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, newPassword);
+            pstmt.setString(2, userId);
+
+            int rowsUpdated = pstmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("비밀번호가 성공적으로 수정되었습니다!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    //Delete -> Update 활성화유무를 확인할 수 있는 메서드
+    public static void updateUserActive(String userId, String userActive) {
+        Connection conn = null; // Connection 객체 선언
+        String sql1 = "UPDATE USER SET user_active = ? WHERE user_id = ?";
+        String sql2 = "UPDATE MEMBERSHIP SET user_active = ? WHERE user_id = ?";
+        try {
+            conn = MySqlConnect.MySqlConnect();
+            PreparedStatement pstmt1 = conn.prepareStatement(sql);
+            PreparedStatement pstmt2 = conn.prepareStatement(sql);
+
+            pstmt1.setString(1, userActive); // 활성화 0 or 1
+            pstmt1.setString(2, userId); // 사용자 ID 설정
+
+            pstmt2.setString(1, userActive);
+            pstmt2.setString(2, userId);
+
+            pstmt1.executeUpdate();
+            pstmt2.executeUpdate();
+
 
             int rowsUpdated = pstmt.executeUpdate();
             if (rowsUpdated > 0) {
@@ -93,71 +185,10 @@ public class UserDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+
+
         }
     }
+
+
 }
-
-
-//user_account_balance 메서드
-
-
-//    // 로그인 메서드
-//    public static boolean loginUser(String userId, String userPassword) {
-//        String sql = "SELECT * FROM USER WHERE user_id = ? AND user_password = ?";
-//        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setString(1, userId);
-//            pstmt.setString(2, userPassword);
-//
-//            ResultSet rs = pstmt.executeQuery();
-//            return rs.next(); // 결과가 있으면 로그인 성공
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
-//    }
-//
-//    // 비밀번호 변경 메서드
-//    public static void updateUserPassword(String userId, String newPassword) {
-//        String sql = "UPDATE USER SET user_password = ? WHERE user_id = ?";
-//        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setString(1, newPassword);
-//            pstmt.setString(2, userId);
-//
-//            int rowsUpdated = pstmt.executeUpdate();
-//            if (rowsUpdated > 0) {
-//                System.out.println("비밀번호가 성공적으로 변경되었습니다.");
-//            } else {
-//                System.out.println("해당 사용자 ID가 존재하지 않습니다.");
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    // 사용자 삭제 메서드
-//    public static void deleteUser(String userId) {
-//        String sql = "DELETE FROM USER WHERE user_id = ?";
-//        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setString(1, userId);
-//
-//            int rowsDeleted = pstmt.executeUpdate();
-//            if (rowsDeleted > 0) {
-//                System.out.println("사용자가 성공적으로 삭제되었습니다.");
-//            } else {
-//                System.out.println("해당 사용자 ID가 존재하지 않습니다.");
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//
-//    }
-//}
