37cea8b71da07688e390b851d4a9bca3318f0556
SungJin Park <pk942@naver.com>
2024-08-02 11:49:51 +0900

    성진-membershiplist 한번에 입금 기능 구현 자동이체 예정

--- src/main/java/TikkeulMountainApp/fund/fundMainApp.java
+++ src/main/java/TikkeulMountainApp/fund/fundMainApp.java
@@ -1,11 +1,14 @@
 package TikkeulMountainApp.fund;
 
+import java.sql.SQLException;
+
 public class fundMainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        FundService.deposit("pk999",1);
+        //FundService.withdraw("pk999",1);
 
+        FundService.depositAtOnce();
 
     }
 }
