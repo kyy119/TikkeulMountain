7462017f10c1fac7cdabe5e65d4f118f58b02f5d
suhueon <degree.h0528@gmail.com>
2024-08-02 10:55:49 +0900

    아스키코드 클래스 생성 쇼카테고리 메서드 추가

--- src/main/java/TikkeulMountainApp/party/PartyApp.java
+++ src/main/java/TikkeulMountainApp/party/PartyApp.java
@@ -1,27 +1,38 @@
 package TikkeulMountainApp.party;
 
 import TikkeulMountainApp.util.MySqlConnect;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PartyApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         PartyService ps = new PartyService();
         System.out.println(
             MySqlConnect.url + " \nuser : " + MySqlConnect.user + "\npw : "
                 + MySqlConnect.password);
 //        Scanner sc = new Scanner(System.in);
-//        System.out.print("모임 이름 : ");
-//        String name = sc.next();
-//        System.out.print("계좌 비밀 번호 : ");
-//        String pw = sc.next();
 //        System.out.print("카테고리 : ");
 //        String cate = sc.next();
-//        PartyService.createParty(name, pw, cate);
-        PartyService.createParty("중국여행", "1211", "여행");
-        ps.updatePartyBalance(3,55500);
-
-        int balance = ps.getPartyBalance(3);
-        System.out.println(balance);
+//        System.out.print("모임 이름 : ");
+//        String name = sc.next();
+//        System.out.print("dailypay : ");
+//        String dailypay = sc.next();
+//        System.out.print("계좌 비밀 번호 : ");
+//        String pw = sc.next();
+//
+//        PartyService.createParty(cate, name, Integer.parseInt(dailypay), pw);
+////        PartyService.createParty("여행","중국여행", 400, "1211");
+//        ps.updatePartyBalance(3,55500);
+//
+//        int balance = ps.getPartyBalance(3);
+//        System.out.println(balance);
+        ArrayList<MemberShip> arr = PartyService.getMemberList();
+        for(MemberShip memberShip : arr){
+            System.out.println(memberShip.getUserId());
+            System.out.println(memberShip.getPartyId());
+            System.out.println(memberShip.getDailyPay());
+        }
     }
 }
