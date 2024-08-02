2d77bd4c0c7b3d7396817431a9abb02cd7deb5cd
suhueon <degree.h0528@gmail.com>
2024-08-02 11:39:16 +0900

    기능수정

--- src/main/java/TikkeulMountainApp/util/MySqlConnect.java
+++ src/main/java/TikkeulMountainApp/util/MySqlConnect.java
@@ -16,7 +16,7 @@ public class MySqlConnect {
         try {
             conn = DriverManager.getConnection(url,user,password);
             if (conn != null) {
-                System.out.println("연결 성공");
+//b                System.out.println("연결 성공");
                 return conn;
             }
         } catch(SQLException e){
